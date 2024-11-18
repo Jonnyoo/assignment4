e4267ad05ad5ce10d608cd9ab28910c3f8ae54c3
[Jonathan <[jonnyzgg@gmail.com]>
2024-11-17 23:05:06 -0500

    removed unnessary code in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React from 'react'
 import './App.css'
 import AmbiguousCase from './Components/AmbiguousCase'
 import HeronsFormula from './Components/HeronsFormula'
@@ -6,17 +6,13 @@ import NewtonsMethod from './Components/NewtonsMethod'
 import PolynomialFunction from './Components/PolynomialFunction'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div className="container">
-        <HeronsFormula/>
-        <AmbiguousCase />
-        <NewtonsMethod />
-        <PolynomialFunction />
-      </div>
-    </>
+    <div className="container">
+      <HeronsFormula />
+      <AmbiguousCase />
+      <NewtonsMethod />
+      <PolynomialFunction />
+    </div>
   )
 }
 
